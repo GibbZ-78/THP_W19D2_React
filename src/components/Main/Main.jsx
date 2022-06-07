a669e15c237518c06d36d97c4dd8f2e84a19258e
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-06-07 23:42:35 +0200

    Attempted a 1st connection between React 'Main' component and the Railsful API...

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,15 +5,19 @@ function Main() {
   return (
     <main className="App-header">
       <p>This is the main part of the web page</p>
-      <p>{
+      <p>*** DEBUT ***</p>
+      <ul>{
         fetch(myURL)
         .then(response => response.json())
         .then(data => {
+          let myTmpHTML;
           for (const article of data.articles) {
-          
+            myTmpHTML += `<li>${article.title}</li>`;
           }
+          return myTmpHTML;
         }).catch(console.error)
-      }</p>
+      }</ul>
+      <p>*** FIN ***</p>
     </main>
   );
 }
